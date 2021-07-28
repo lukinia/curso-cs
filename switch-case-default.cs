4c146f02c7a4f4c1fa9bfda33d6805e980d2605f
Lucas Corrêa <63137745+lucacorrea@users.noreply.github.com>
2021-07-28 15:44:48 -0300

    Utilizando Switch, Case e Default.

---
+++ switch-case-default.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Course
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            string dia;
+            
+            switch (x)
+            {
+                case 1:
+                    dia = "Domingo";
+                    break;
+                case 2:
+                    dia = "Segunda";
+                    break;
+                case 3:
+                    dia = "Terça";
+                    break;
+                case 4:
+                    dia = "Quarta";
+                    break;
+                case 5:
+                    dia = "Quinta";
+                    break;
+                case 6:
+                    dia = "Sexta";
+                    break;
+                case 7:
+                    dia = "Sábado";
+                    break;
+                default:
+                    dia = "Nenhum";
+                    break;
+            }
+            Console.WriteLine(dia);
+        }
+    }
+}
